95668c9a4314ddfd06d5e61ceaf636ca8c98cf84
DevKimDN <kimtrongbk@gmail.com>
2021-06-11 15:01:14 +0100

    version --++

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -26,7 +26,7 @@ export function  Counter() {
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
-          -
+          ---
         </button>
         <span className={styles.value}>{count}</span>
         <button
@@ -34,7 +34,7 @@ export function  Counter() {
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
-          +
+          +++
         </button>
       </div>
       <div className={styles.row}>
